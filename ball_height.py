4183a7eb5d8f15cab3d01a90f951b8668e20e869
hyuk-dev <139575548+hyuk-dev@users.noreply.github.com>
2024-01-25 02:30:22 +0000

    공의 높이, 제곱 테이블

---
+++ ball_height.py
@@ -0,0 +1,7 @@
+count = 1
+ball_height = 60.0
+while count <=10:
+  print(str(count) + ' ' + str(round(ball_height,4)))
+  count+=1
+  ball_height*=0.6
+  
---
+++ square_table.py
@@ -0,0 +1,4 @@
+square_table = int(input('Enter a num: '))
+for i in range(1, square_table + 1):
+  print(str(i) + '' + str(i**2))
+  
